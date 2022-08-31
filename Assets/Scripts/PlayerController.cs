66d8bc4ba42bec80089f44749734a66edb96de19
Tanner Denti <tannerwdenti@gmail.com>
2022-08-31 16:00:10 -0600

    Polished player movement, and created better mechanics for movement boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,35 +6,22 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
+
     private float speed = 10;
+    private float impulseStrength = 2000;
 
-    private float horizontalInput;
-    private float fowardInput;
+    private float leftBound = -7.4f;
+    private float rightBound = 10.2f;
+    private float fowardBound = 5.0f;
+    private float backwardBound = 21.0f;
 
-    private float leftBound;
-    private float rightBound;
-    private float fowardBound;
-    private float backwardBound;
-
-    private float impulseStrength = 250;
-    public float buffer = 0.0f;
-
-
-    private Collider rightWall;
-    private Collider leftWall;
-    private Collider playerCollider;
-
-    public bool onCollision = false;
+    private bool canMoveRight = true;
+    private bool canMoveLeft = true;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        //playerCollider = GetComponent<Collider>();
-        //rightWall = GameObject.Find("RightWallCollider").GetComponent<Collider>();
-        //leftWall = GameObject.Find("LeftWallCollider").GetComponent<Collider>();
-
-
     }
 
     // Update is called once per frame
@@ -42,69 +29,72 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
         ConstrainPlayerPosition();
+        reactivateMovement();
     }
 
+    // Move around with WASD
     void MovePlayer()
     {
-        if (!onCollision)
-        {
-            // Gets horizontal and vertical 
-            horizontalInput = Input.GetAxis("Horizontal");
-            fowardInput = Input.GetAxis("Vertical");
 
-            // Had to swap the the first parameters because the car asset isn't oriented properly
-            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed * fowardInput);
-            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * horizontalInput);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed);
         }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A) && canMoveLeft)
+        {
+            transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D) && canMoveRight)
+        {
+            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);  
+        }
+
     }
 
+    // Restrict the player from moving too far foward, back, or side to side. Bounce the player off of side walls.
     void ConstrainPlayerPosition()
     {
-        leftBound = -7.5f;
 
-
+        if (transform.position.x < fowardBound)
+        {
+            transform.position = new Vector3(fowardBound, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > backwardBound)
+        {
+            transform.position = new Vector3(backwardBound, transform.position.y, transform.position.z);
+        }
         if (transform.position.z < leftBound)
         {
+            canMoveLeft = false; // If we don't turn off movement in this direction, and the player holds this key down, we get visual jitter.
             transform.position = new Vector3(transform.position.x, transform.position.y, leftBound);
-            // playerRb.AddForce(new Vector3(0, 0, 1) * impulseStrength * Time.deltaTime, ForceMode.Impulse);
+            playerRb.AddForce(new Vector3(0, 0, 1) * impulseStrength * Time.deltaTime, ForceMode.Impulse);
+        }
+        if (transform.position.z > rightBound)
+        {
+            canMoveRight = false; // If we don't turn off movement in this direction, and the player holds this key down, we get visual jitter.
+            transform.position = new Vector3(transform.position.x, transform.position.y, rightBound);
+            playerRb.AddForce(new Vector3(0, 0, -1) * impulseStrength * Time.deltaTime, ForceMode.Impulse);
         }
 
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-
-    //    if (collision.gameObject.CompareTag("leftWall"))
-    //    {
-    //        onCollision = true;
-    //        playerRb.AddForce(new Vector3(0, 0, 1) * impulseStrength * Time.deltaTime, ForceMode.Impulse);
-    //    }
-
-    //    if (collision.gameObject.CompareTag("rightWall"))
-    //    {
-    //        onCollision = true;
-    //        playerRb.AddForce(new Vector3(0, 0, -1) * impulseStrength * Time.deltaTime, ForceMode.Impulse);
-    //    }
+    // Turn player movement back on after they have been "bounced" off of side walls. 
+    void reactivateMovement()
+    {
+        if (transform.position.z >= -6.0 && transform.position.z <= 8.8f)
+        {
+            playerRb.velocity = new Vector3(0, 0, 0); // Stops the applied force, which can be inconsistent. Will be stopped on the z axis at -6 or 8.8.
+            canMoveLeft = true;
+            canMoveRight = true;
+        }
         
-    //}
-
-
-    //private void OnCollisionExit(Collision collision)
-    //{ 
-    //    if (collision.gameObject.CompareTag("leftWall") || collision.gameObject.CompareTag("rightWall"))
-    //    {
-    //        while (buffer <= 100000.0f)
-    //        {
-    //            buffer += Time.deltaTime;
-    //        }
-    //        if (buffer >= 100000.0f)
-    //        {
-    //            buffer = 0.0f;
-    //            onCollision = false;
-    //            Destroy(gameObject);
-    //        } 
-    //    }
-        
-    //}
+    }
 
 }
